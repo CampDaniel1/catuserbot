bbf9ee04d7e08f0f5404dd207065e82b83e1a53c
CampDaniel1 <149169638+CampDaniel1@users.noreply.github.com>
2023-11-17 22:31:18 +0530

    Update and rename exampleconfig.py to config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 24185140
+    API_HASH = "a4ffcafc1a4b57f9993b04321babacd5"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "UserBot"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://dvatlice:xOO8WX4ut2P3t2wufI1hWXdlFktA0wyy@flora.db.elephantsql.com/dvatlice"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1BVtsOI4BuyHK5qs-ObdHA5w76_BhuIMOgZ_JTjSb9OVdfLjl5W2v_zzvf76SBTk02W8dPJvV_K0crt2KIz8xB_uo_ZQOXRwe9f05uAz8qAOaTaKIXNAwafRuF3qpph7Ysapyu4psULKVd7-za8Bz7yWuoEFe774Azyz4ukjYs7QPDJHRRl0cwju20Cgh-CwaDcnfV0TrPmVdWLAmTyNxEyOVpgBcA-uKJKusLhLFTEfSHwXoj-KQ9OdFAyTbKghP_arYPc6m63TsntIgmQDb3BrD1d7CB8H_m207te-VhJQeCGARwT1NZ_Xxm29l3DjtXRRusKr23cN4uNHf29G1UYO3Kc7M_uo="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "6728820895:AAFouRa3uPjz7y-NbmVf5cvn1hYmBOLvm2w"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -1002132527207
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
@@ -22,4 +22,4 @@ class Development(Config):
     # External plugins repo
     EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
     # if you need badcat plugins set "True"
-    BADCAT = "False"
+    BADCAT = "True"
